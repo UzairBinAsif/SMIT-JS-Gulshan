7676066e10d2473520e3a6fd07e2ffe9d4140857
UzairBinAsif <shaikhuzairasif14@gmail.com>
2025-08-22 23:29:52 +0500

    add assignment 1 ch 1 - 2

---
+++ assignment 1/app.js
@@ -0,0 +1,65 @@
+// Ch 1
+// 1
+alert("Welcome");
+
+// 2
+alert("Error! Please enter a valid password.");
+
+// 3
+alert(`Welcome to JS land...
+Happy Coding!`);
+
+// 4
+alert(`Welcome to JS land...`);    
+alert(`Happy Coding!`);
+
+// 5
+console.log("Hello... I can run JS through my web browser's console.");
+
+// 6
+alert("Hello World!");
+
+// Ch 2
+// 1
+var username;
+
+// 2
+var myName;
+myName = "Uzair Bin Asif";
+
+// 3
+var message;
+message = "Hello World";
+alert(message)
+
+// 4
+var userName = "Uzair Bin Asif";
+var userAge = 18;
+var userCourse = "Web & Mobile App Development";
+
+alert(userName);
+alert(userAge);
+alert(userCourse);
+
+// 5
+alert(`PIZZA
+PIZZ
+PIZ
+PI
+P`);
+
+// 6
+var email = "example@example.com";
+alert(`My email address is ${email}`);
+
+// 7
+var book = "A smarter way to learn JavaScript";
+alert(`I am trying to learn from the book ${book}`);
+
+// 8
+document.write("Yah, I can write HTML content through Javascript.<br>");
+
+// 9
+var string1 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(string1);
+document.write(string1);
